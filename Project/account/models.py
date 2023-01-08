82ab1f9ef8f45918c1d9873c8c41a87ac247ac93
Dmitry <dmitrypomishin@gmail.com>
2023-01-08 18:03:15 +0100

    Filled in models for db and made stable migration

--- Project/account/models.py
+++ Project/account/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, 'Director'),
+    (admin, 'Manager'),
+    (cook, 'Cook'),
+    (cashier, 'Cashier'),
+    (cleaner, 'Cleaner')
+]
+
+
+class Staff(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
+    labor_contract = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+    composition = models.TextField(default='Composition is not indicated')
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', through='ProductOrder')
+
+
+class ProductOrder(models.Model):
+    Order = models.ForeignKey('Product', on_delete=models.CASCADE)
+    Product = models.ForeignKey('Order', on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
